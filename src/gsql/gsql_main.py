ed011f41421e8c974e031802325a87976ec5708f
Amy <amy@amyip.net>
2025-01-15 22:14:20 -0800

    Create pylint.yml (#6)
    This patch adds linting and fixes many PEP8 violations.
    
    Signed-off-by: Amy Parker <amy@amyip.net>

--- src/gsql/gsql_main.py
+++ src/gsql/gsql_main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+"""Unused template module to allow the bootstrapper to run global SQL files."""
+
 if __name__ == "__main__":
     pass
